5628282188cc06688026941753244adc6d0a5250
SpecialRoyalty <nataliatupalskaparis@gmail.com>
2026-04-10 13:19:40 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,7 +3,13 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: process.env.DATABASE_URL.includes("localhost")
+    ? false
+    : { rejectUnauthorized: false }
+});
+
+pool.on("error", (err) => {
+  console.error("PostgreSQL error:", err);
 });
 
 module.exports = pool;
